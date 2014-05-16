1175fdd41c076bdbcfff7de40b3f2980a3dd05f9
SergioDiez <sdgodam@gmail.com>
2014-05-16 12:22:06 +0200

    refactorizado el programa para que sea sencillo hacer un cambio de idioma

---
+++ Option.java
@@ -0,0 +1,10 @@
+
+/**
+ * Enumeration class Option - write a description of the enum class here
+ * 
+ * @author Sergio Diez
+ */
+public enum Option
+{
+    GO, QUIT, HELP, LOOK, EAT, BACK, ITEMS, TAKE, DROP, UNKNOWN
+}
