c1ccc661ef0222eeba40e15fc8dc56e0393b06b5
SergioDiez <sdgodam@gmail.com>
2014-05-17 20:16:58 +0200

    modificada CommandWorks para que funcione con los cambios previos y agregados comentarios en clase Option olvidados en el paso previo

--- Option.java
+++ Option.java
@@ -9,12 +9,20 @@ public enum Option
     GO("ir"), QUIT("cerrar"), HELP("ayuda"), LOOK("mirar"), EAT("comer"), BACK("atras"), ITEMS("objetos"),
     TAKE("coger"), DROP("dejar"), UNKNOWN("desconocido");
     
+    //atribute
     private String command;
     
+    /*
+     * Constructor of the Option objects
+     * @param command - The command that is associated
+     */
     Option(String command){
         this.command = command;
     }
     
+    /*
+     * @return the Command associated to the Option object
+     */
     public String getCommand(){
         return command;
     }
